f695c3b613800a2c3305483492d83cb90502a422
Ryder25 <tasevp@yahoo.com>
2016-05-12 19:31:38 -0700

    Cleaned up the BH3 exporter to simplify the code a bit

--- _Blender Scripts/riseofnations/blender/bh3fileexporter.py
+++ _Blender Scripts/riseofnations/blender/bh3fileexporter.py
@@ -10,10 +10,11 @@ class BH3FileExporter:
         self._file = None
         self._model = None
         self._mesh = None
-        self._vertex_count = 0
-        self._normals = []
         self._uv_loops = None
-        self._vertex_old_new_index = dict()
+        self._normals = []
+
+        self._vertex_count = 0
+        self._vertex_index = 0
         self._traversed_loops = set()
 
     def save(self, ctx, filename):
@@ -23,7 +24,6 @@ class BH3FileExporter:
         # self._mesh = object_.data
         self._mesh = self._model.to_mesh(ctx.scene, True, 'PREVIEW', True)
         self._mesh.calc_normals_split()
-        self._vertex_count = len(self._mesh.vertices)
 
         uv_layer = self._mesh.uv_layers.active
         self._uv_loops = uv_layer.data if uv_layer is not None else None
@@ -43,7 +43,7 @@ class BH3FileExporter:
         bpy.ops.object.mode_set(mode='EDIT')
         armature = skin.data
 
-        self._file.root_bone = self._create_bh3_bones(armature.edit_bones[0], [0])
+        self._file.root_bone = self._create_bh3_bones(armature.edit_bones[0])
 
         for face in self._mesh.polygons:
             self._file.faces.append([face.vertices[0],
@@ -59,11 +59,13 @@ class BH3FileExporter:
         print('Export took {:f} seconds'.format(process_time() - start_time))
         return {'FINISHED'}
 
-    def _create_bh3_bones(self, abone, vertex_index):
+    def _create_bh3_bones(self, abone):
         bone = BH3Bone()
         bone.name = abone.name
 
         bone_vertices = dict()
+        starting_vertex_count = self._vertex_count
+
         transform_inverse = abone.matrix.inverted()
         nrm_mtx = transform_inverse.transposed().inverted()
 
@@ -71,61 +73,59 @@ class BH3FileExporter:
             if not (loop.index in self._traversed_loops):
                 new_vertex_index = -1
 
-                if not (loop.vertex_index in self._vertex_old_new_index):
+                if not (loop.vertex_index in bone_vertices):
                     # If vertex does not already belong to a bone
                     for vg in self._mesh.vertices[loop.vertex_index].groups:
                         # only one bone may take a vertex, so break after first find
                         if self._model.vertex_groups[vg.group].name == abone.name:
-                            new_vertex_index = len(self._vertex_old_new_index)
-                            bone_vertices[loop.vertex_index] = dict()
+                            new_vertex_index = self._vertex_count
+                            self._vertex_count += 1
                             break
 
-                if loop.vertex_index in bone_vertices:
+                if new_vertex_index >= 0:
+                    # First time this vertex has been seen
                     self._traversed_loops.add(loop.index)
-                    if new_vertex_index >= 0:
-                        # First time this vertex has been seen
+                    self._file.vertices.append(
+                        list(transform_inverse * self._mesh.vertices[loop.vertex_index].co))
+                    self._file.normals.append(list(nrm_mtx * self._normals[loop.vertex_index]))
+
+                    if self._uv_loops:
+                        uv = tuple(self._uv_loops[loop.index].uv)
+                        self._file.uvs.append(list(uv))
+                        bone_vertices[loop.vertex_index] = {uv: new_vertex_index}
+                    else:
+                        self._file.uvs.append([0, 1])
+                        bone_vertices[loop.vertex_index] = new_vertex_index
+
+                    loop.vertex_index = new_vertex_index
+                elif loop.vertex_index in bone_vertices:
+                    # Second+ time we see this vertex index
+                    self._traversed_loops.add(loop.index)
+                    if not self._uv_loops:
+                        # No UVs, just update the loop vertex_index
+                        loop.vertex_index = bone_vertices[loop.vertex_index]
+                        continue
+
+                    uv_dict = bone_vertices[loop.vertex_index]
+                    uv = tuple(self._uv_loops[loop.index].uv)
+
+                    # print(str(loop.vertex_index) + ' ' + str(uv_dict))
+
+                    if uv in uv_dict:
+                        # We have UVs, but they have been accounted for
+                        loop.vertex_index = uv_dict[uv]
+                    else:
+                        # Unique UV, copy the vertex to avoid seams
+                        print('Hellooooooooooooooooooooooooooooooooooooooooooooooo')
                         self._file.vertices.append(
                             list(transform_inverse * self._mesh.vertices[loop.vertex_index].co))
                         self._file.normals.append(list(nrm_mtx * self._normals[loop.vertex_index]))
+                        self._file.uvs.append(list(uv))
 
-                        if self._uv_loops:
-                            uv = tuple(self._uv_loops[loop.index].uv)
-                            self._file.uvs.append(list(uv))
-                            bone_vertices[loop.vertex_index][uv] = new_vertex_index
-                        else:
-                            self._file.uvs.append([0, 1])
-
-                        self._vertex_old_new_index[loop.vertex_index] = new_vertex_index
+                        new_vertex_index = self._vertex_count
+                        self._vertex_count += 1
+                        uv_dict[uv] = new_vertex_index
                         loop.vertex_index = new_vertex_index
-                    else:
-                        # Second+ time we see this vertex index
-                        if not self._uv_loops:
-                            loop.vertex_index = self._vertex_old_new_index[loop.vertex_index]
-                            continue
-
-                        uv_dict = bone_vertices[loop.vertex_index]
-                        uv = tuple(self._uv_loops[loop.index].uv)
-
-                        # print(str(loop.vertex_index) + ' ' + str(uv_dict))
-
-                        if uv in uv_dict:
-                            # We have UVs, but they have been accounted for
-                            loop.vertex_index = uv_dict[uv]
-                        else:
-                            # Unique UV, copy the vertex to avoid seams
-                            print('Hellooooooooooooooooooooooooooooooooooooooooooooooo')
-                            self._file.vertices.append(
-                                list(transform_inverse * self._mesh.vertices[loop.vertex_index].co))
-                            self._file.normals.append(list(nrm_mtx * self._normals[loop.vertex_index]))
-                            self._file.uvs.append(list(uv))
-
-                            new_vertex_index = len(self._vertex_old_new_index)
-                            bone_vertices[self._vertex_count] = dict()
-                            uv_dict[uv] = new_vertex_index
-
-                            self._vertex_old_new_index[self._vertex_count] = new_vertex_index
-                            loop.vertex_index = new_vertex_index
-                            self._vertex_count += 1
 
         # Calculate the local rotation and position for the bone
         if abone.parent:
@@ -136,12 +136,12 @@ class BH3FileExporter:
             bone.rotation = list(abone.matrix.transposed().to_quaternion())
             bone.position = list(abone.matrix.to_translation())
 
-        bone.vertex_count = len(bone_vertices)
-        bone.vertex_index = vertex_index[0]
-        vertex_index[0] += bone.vertex_count
+        bone.vertex_count = self._vertex_count - starting_vertex_count
+        bone.vertex_index = self._vertex_index
+        self._vertex_index += bone.vertex_count
 
         for achild in abone.children:
-            child = self._create_bh3_bones(achild, vertex_index)
+            child = self._create_bh3_bones(achild)
             child.parent = bone
             bone.children.append(child)
 
