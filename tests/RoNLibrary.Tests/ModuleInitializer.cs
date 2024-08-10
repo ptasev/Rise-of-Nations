5552623ec4d91ae70349b6e2dea0379f386ef0cd
Petar Tasev <ptasev@gmail.com>
2024-08-09 21:36:10 -0700

    Add Verify package

---
+++ tests/RoNLibrary.Tests/ModuleInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace RoNLibrary.Tests;
+
+public static class ModuleInitializer
+{
+    [ModuleInitializer]
+    public static void Initialize() =>
+        VerifyDiffPlex.Initialize();
+
+    [ModuleInitializer]
+    public static void OtherInitialize()
+    {
+        VerifierSettings.InitializePlugins();
+        VerifierSettings.ScrubLinesContaining("DiffEngineTray");
+        VerifierSettings.IgnoreStackTrace();
+    }
+}
